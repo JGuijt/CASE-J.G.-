22e3cc8f71e282a2a801d329080399c120e5e970
jaap guijt <jaapguijt@hotmail.com>
2021-03-22 14:33:22 +0100

    Set up db

---
+++ EindCaseBackEnd/Models/CursusContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EindCaseBackEnd.Models
+{
+    public class CursusContext : DbContext
+    {
+       
+        
+        public CursusContext(DbContextOptions<CursusContext> options) : base(options)
+        { }
+
+        
+        DbSet<Cursus> Cursussen {get; set;}
+        DbSet<CursusInstantie> CursusInstanties { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cursus>()
+                .HasKey(c => c.Code);
+            modelBuilder.Entity<CursusInstantie>()
+                .HasKey(ci => new { ci.StartDatum, ci.Code });
+        }
+
+
+
+    }
+}
---
+++ EindCaseBackEnd/Models/CursusInstantie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EindCaseBackEnd.Models
+{
+    public class CursusInstantie
+    {
+        public DateTime StartDatum {get; set;}
+        public string Code { get; set; }
+
+        public Cursus Cursus { get; set; }
+
+    }
+}
