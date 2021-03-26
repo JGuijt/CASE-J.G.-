3866354609f0ef604c3f9bc5b9278af189520cd2
JGuijt <74177687+JGuijt@users.noreply.github.com>
2021-03-26 11:12:16 +0100

    added backend full code

---
+++ BackEnd/code/Reader/FileReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Reader
+{
+    public class FileReader
+    {
+        
+        public async Task<string> ReadFile(IFormFile file)
+        {
+
+        
+        StringBuilder result = new StringBuilder();
+        using(StreamReader reader = new StreamReader(file.OpenReadStream()))
+        {
+        while (reader.Peek() >= 0)
+            { result.AppendLine(await reader.ReadLineAsync()); }
+        }
+        return result.ToString();
+        }
+
+
+
+
+
+
+
+
+    }
+}
