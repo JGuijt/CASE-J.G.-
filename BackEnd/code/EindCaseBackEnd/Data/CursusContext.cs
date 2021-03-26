7b5ffa5e235e945c1c0504a70f1abc761922a747
JGuijt <74177687+JGuijt@users.noreply.github.com>
2021-03-26 11:20:42 +0100

    add backing up

---
+++ BackEnd/code/EindCaseBackEnd/Data/CursusContext.cs
@@ -0,0 +1,44 @@
+﻿using EindCaseBackEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EindCaseBackEnd.Data
+{
+    public class CursusContext : DbContext
+    {
+       
+        
+        public CursusContext(DbContextOptions<CursusContext> options) : base(options)
+        { }
+
+        
+        public DbSet<Cursus> Cursussen {get; set;}
+        public DbSet<CursusInstantie> CursusInstanties { get; set; }
+
+
+
+
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cursus>()
+                .Property(c => c.Titel)
+                .HasMaxLength(300);
+            modelBuilder.Entity<Cursus>()
+                .Property(c => c.Code)
+                .HasMaxLength(10);
+            
+            modelBuilder.Entity<CursusInstantie>()
+                .Property(ci => ci.StartDatum)
+                .HasColumnType("date");
+        }
+
+
+
+    }
+}
---
+++ BackEnd/code/EindCaseBackEnd/Migrations/20210323085229_SetPropReq.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EindCaseBackEnd.Migrations
+{
+    public partial class SetPropReq : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titel",
+                table: "Cursussen",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Cursussen",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDatum",
+                table: "CursusInstanties",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titel",
+                table: "Cursussen",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Cursussen",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDatum",
+                table: "CursusInstanties",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+    }
+}
